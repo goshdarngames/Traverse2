c13645c46eb93b52cad4c5bb98c9edd59b1ddd0f
Gosh Darn Games <1109935+goshdarngames@users.noreply.github.com>
2020-04-28 15:00:39 +0100

    Get prolog through an AJAX request

--- javascript/traverse_init.js
+++ javascript/traverse_init.js
@@ -3,6 +3,26 @@
     traverse.init_prolog = function ()
     {
         window.console.log ( "Initialize traverse prolog..." );
+
+        let request = new XMLHttpRequest ();
+
+        request.open ( 'GET', 'prolog/traverse.pl' );
+
+        request.responseType = 'text';
+
+        request.onload = function ()
+        {
+            let s = pl.create ();
+            s.consult ( request.response );
+
+            s.query ("likes(sam,X).");
+
+            let cb = console.log;
+
+            s.answer ( cb );
+        };
+
+        request.send ();
     };
     
 } ( window.traverse = window.traverse || {} ))
