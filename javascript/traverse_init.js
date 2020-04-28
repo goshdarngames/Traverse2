3c3093a571090137fc238e3b848f09ebdb13a257
Gosh Darn Games <1109935+goshdarngames@users.noreply.github.com>
2020-04-28 14:17:28 +0100

    Create project structure

---
+++ javascript/traverse_init.js
@@ -0,0 +1,8 @@
+( function ( traverse, undefined )
+{
+    traverse.init_prolog = function ()
+    {
+        window.console.log ( "Initialize traverse prolog..." );
+    };
+    
+} ( window.traverse = window.traverse || {} ))
