b5d61a2c561ffb0a4568e8511a511c5ca1a60289
Gosh Darn Games <1109935+goshdarngames@users.noreply.github.com>
2020-04-28 21:29:54 +0100

    Create pixi graphics

--- javascript/traverse_init.js
+++ javascript/traverse_init.js
@@ -27,5 +27,28 @@
 
         request.send ();
     };
+
+    traverse.init_pixi = function ( pixi, document )
+    {
+        let type="WebGL";
+        
+        if ( !pixi.utils.isWebGLSupported () )
+        {
+            type = "canvas";
+        }
+
+        pixi.utils.sayHello ( type );
+
+        pixi_app = new pixi.Application (
+        {
+            width  : 800,
+            height : 800
+        });
+
+        document.body.appendChild ( pixi_app.view );
+
+        return pixi_app;
+    };
+
     
 } ( window.traverse = window.traverse || {} ))
---
+++ javascript/traverse_main_loop.js
@@ -0,0 +1,28 @@
+( function ( traverse, undefined )
+{
+    traverse.mainLoop = function ( traverse_data )
+    {
+        if ( traverse_data.tick == undefined )
+        {
+            traverse_data.tick = start_state;
+        }
+
+        traverse_data.tick ( traverse_data );
+    };
+
+    let start_state = function ( traverse_data )
+    {
+        traverse_data.wall_graphics = new traverse_data.pixi.Graphics ();
+
+        traverse_data.wall_graphics.beginFill(0xFFFF00);
+
+        traverse_data.wall_graphics.lineStyle( 5, 0xFF0000);
+
+        traverse_data.wall_graphics.drawRect (0,0, 300,200 );
+
+        traverse_data.pixi_app.stage.addChild ( traverse_data.wall_graphics );
+    };
+
+    //TODO - Add a function to create wall graphics from prolog rules
+} ( window.traverse = window.traverse || {} ))
+
