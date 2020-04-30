ad64863e415e62fe06c54316505083efca0b3630
Gosh Darn Games <1109935+goshdarngames@users.noreply.github.com>
2020-04-30 14:41:37 +0100

    Implement JS tabs.
    ( may be replaced with vue )

---
+++ javascript/traverse_dom_input.js
@@ -0,0 +1,34 @@
+//Handles inputs from buttons / forums from the html document
+
+( function ( traverse, undefined )
+{
+    traverse.activate_tab = function ( event, tab_name )
+    {
+        let control_panel = 
+            document.getElementById ( "control_panel_container" );
+
+        //hide all content
+
+        let content = control_panel.getElementsByClassName ( "tabcontent" );
+
+        Array.from ( content ).forEach ( ( el ) =>
+        {
+            el.style.display = "none";
+        });
+
+        //set all tab header links as inactive (remove CSS class)
+        
+        let tablinks = control_panel.getElementsByClassName ( "tablinks" );
+
+        Array.from ( tablinks ).forEach ( ( el ) =>
+        {
+            el.className = el.className.replace ( " active", "" );
+        });
+
+        //show the selected content and set the button to 'active'
+
+        document.getElementById ( tab_name ).style.display = "block";
+
+        event.currentTarget.className += "active";
+    };
+} ( window.traverse = window.traverse || {} ))
