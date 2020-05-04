44a53f7aee55523282c689a0b74df36750fa13b3
Gosh Darn Games <1109935+goshdarngames@users.noreply.github.com>
2020-05-04 21:04:26 +0100

    Button style

--- javascript/game_states/traverse_title_state.js
+++ javascript/game_states/traverse_title_state.js
@@ -10,17 +10,27 @@
 
             cp.clear_content ();
 
-            let add_menu_button = ( cp, text ) =>
+            let div = document.createElement ( "DIV" );
+
+            div.classList.add ( "flex_column" );
+            div.classList.add ( "flex_centered" );
+            div.classList.add ( "content_pane" );
+
+            cp.add_content ( div );
+
+            let add_menu_button = ( div, text ) =>
             {
                 let button = document.createElement ( "BUTTON" );
                 
                 button.innerHTML = text;
 
-                cp.add_content ( button );
+                button.classList.add ( "column_button" );
+
+                div.appendChild ( button );
             };
 
-            add_menu_button ( cp, "Play" );
-            add_menu_button ( cp, "Create" );
+            add_menu_button ( div, "Play" );
+            add_menu_button ( div, "Create" );
 
             this.tick = () => {};
 
