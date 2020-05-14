d5047e9fc78aaf57c4198f820bcd32548d0f09c8
Gosh Darn Games <1109935+goshdarngames@users.noreply.github.com>
2020-05-14 10:55:47 +0100

    Unique puzzle object test

--- javascript/traverse_puzzle_objects.test.js
+++ javascript/traverse_puzzle_objects.test.js
@@ -6,6 +6,12 @@ let MockOb = function ()
     this.unique = false;
 }
 
+let MockUnique = function ()
+{
+    this.name = "mock_unique";
+    this.unique = true;
+}
+
 test('PuzzleState insert and remove standard', () => 
 {
     let ps = new traverse.PuzzleState ();
@@ -26,5 +32,33 @@ test('PuzzleState insert and remove standard', () =>
     ps.add_object ( p );
 
     expect( ps.get_object_at_pos ( 1, 1 ) ).toBe ( p );
+});
+
+test('PuzzleState insert unique', () => 
+{
+    let ps = new traverse.PuzzleState ();
+
+    let o = new traverse.PuzzleObject ( MockUnique, 1, 1 );
+    let p = new traverse.PuzzleObject ( MockUnique, 1, 3 );
+
+    ps.add_object ( o );
+
+    expect( ps.get_object_at_pos ( 1, 1 ) ).toBe ( o );
+
+    expect ( ps.get_unique_object ( "mock_unique" ).toBe ( o ) );
+
+    expect ( () => ps.add_object ( p ) ).toThrow ();
+
+    ps.remove_unique_object ( "mock_unique" );
+
+    expect( ps.get_object_at_pos ( 1, 1 ) ).toBeUndefined ();
+    
+    expect ( ps.get_unique_object ( "mock_unique" ).toBeUndefined () );
+
+    ps.add_object ( p );
+
+    expect( ps.get_object_at_pos ( 1, 3 ) ).toBe ( p );
+
+    expect ( ps.get_unique_object ( "mock_unique" ).toBe ( p ) );
 });
 
