d9096578b3a9a5a602a36691db015c57ca1b7c73
Gosh Darn Games <1109935+goshdarngames@users.noreply.github.com>
2020-05-19 15:10:15 +0100

    Prolog object tests

--- tests/prolog_puzzle.test.js
+++ tests/prolog_puzzle.test.js
@@ -4,6 +4,13 @@ let fs = require("fs").promises;
 
 require ( "../javascript/traverse_prolog" );
 
+//Wrapping the prolog answer in a promise allows jest to wait on the
+//asynchronous code from the prolog system
+let answer_promise = ( session ) => new Promise ( resolve =>
+{
+    session.answer ( ( a ) => resolve ( a ) );
+});
+
 test ( "Get libs", async () =>
 {
     expect ( pl ).toBeDefined ();
@@ -13,5 +20,21 @@ test ( "Get libs", async () =>
     const pl_data = await fs.readFile (  
         "./prolog/traverse_rules.pl", "utf8" );
 
-    console.log ( pl_data );
+    let session = pl.create ();
+
+    let answer = undefined;
+
+    session.consult ( pl_data );
+
+    session.query ( "object(bad_object)." );
+    answer = await answer_promise ( session );
+
+    for ( const query of [ "boo", "bogey", "wall" ] )
+    {
+        session.query ( `(${query}).` );
+        answer = await answer_promise ( session );
+
+        expect ( answer ).toBeDefined ();
+    }
+
 });
