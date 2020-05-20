3568f27e9f3c03433caa14d4d7708529c08ba17c
Gosh Darn Games <1109935+goshdarngames@users.noreply.github.com>
2020-05-20 11:20:00 +0100

    Access prolog using promises

--- javascript/traverse_prolog.js
+++ javascript/traverse_prolog.js
@@ -1,26 +1,25 @@
 ( function ( traverse, undefined )
 {
-    traverse.prolog_verify_puzzle_state = 
-        function ( ps, success_cb, fail_cb, traverse_data )
+    traverse.prolog_verify_puzzle_state = function ( ps, traverse_data )
     {
-        let ps_pl = ps.get_prolog ();
-
-        let query = `puzzle_problem(${ps_pl},X).`;
-
-        traverse_data.assets.rules_pl.query ( query );
-
-        traverse_data.assets.rules_pl.answer ( ( a ) =>
+        return new Promise ( ( resolve, reject ) =>
         {
-            //TODO move prolog queries to the prolog js file
+            let ps_pl = ps.get_prolog ();
 
-            let msg_array = a.lookup ("X").toJavaScript();
+            let query = `puzzle_problem(${ps_pl},X).`;
 
-            let msg = convert_prolog_string ( msg_array );
+            traverse_data.assets.rules_pl.query ( query );
 
-            fail_cb ( msg );
+            traverse_data.assets.rules_pl.answer ( ( a ) =>
+            {
+                let msg_array = a.lookup ("X").toJavaScript();
 
-        } );
+                let msg = convert_prolog_string ( msg_array );
 
+                resolve ( msg );
+
+            });
+        });
     };
 
     let convert_prolog_string = 
--- tests/prolog_puzzle.test.js
+++ tests/prolog_puzzle.test.js
@@ -1,30 +1,62 @@
+/**
+ * prolog_puzzle.test.js
+ *
+ * Tests the functions used to check the puzzle state between prolog
+ * and javascript.
+ */
+
 let pl = require ( "tau-prolog" );
 
 let fs = require("fs").promises;
 
+require ( "../javascript/traverse_puzzle_objects" );
 require ( "../javascript/traverse_prolog" );
 
-test ( "Get libs", async () =>
+//Wrapping the prolog answer in a promise allows jest to wait on the
+//asynchronous code from the prolog system
+let answer_promise = ( session ) => new Promise ( resolve =>
+{
+    session.answer ( ( a ) => resolve ( a ) );
+});
+
+let read_pl_data = async function ()
+{
+    let pl_data = await fs.readFile (  
+        "./prolog/traverse_rules.pl", "utf8" );
+
+    return pl_data;
+}
+
+let get_traverse_data = async function ()
+{
+    let session = pl.create ();
+
+    let pl_data = await read_pl_data ();
+
+    session.consult ( pl_data );
+
+    let traverse_data = 
+    {
+        assets :
+        {
+            rules_pl : session,
+        },
+    };
+
+    return traverse_data;
+};
+
+test ( "Prolog objects defined", async () =>
 {
     expect ( pl ).toBeDefined ();
     
     expect ( traverse ).toBeDefined ();
 
-    const pl_data = await fs.readFile (  
-        "./prolog/traverse_rules.pl", "utf8" );
+    let traverse_data = await get_traverse_data ();
 
-    let session = pl.create ();
+    let session = traverse_data.assets.rules_pl;
 
     let answer = undefined;
-
-    //Wrapping the prolog answer in a promise allows jest to wait on the
-    //asynchronous code from the prolog system
-    let answer_promise = ( session ) => new Promise ( resolve =>
-    {
-        session.answer ( ( a ) => resolve ( a ) );
-    });
-
-    session.consult ( pl_data );
 
     session.query ( "object(bad_object)." );
     answer = await answer_promise ( session );
@@ -39,4 +71,16 @@ test ( "Get libs", async () =>
         expect ( answer ).toBeDefined ();
     }
 
+    
+
+});
+
+test ( "Prolog puzzle_problem", async () =>
+{
+    let traverse_data = await get_traverse_data ();
+    let ps = new traverse.PuzzleState ();
+
+    let a = await traverse.prolog_verify_puzzle_state ( ps, traverse_data );
+
+    expect ( a ).toEqual ( "Prolog said no." );
 });
