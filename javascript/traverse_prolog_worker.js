40d738ea1d8176ff17bfa1558e30b4e7b417baec
Gosh Darn Games <1109935+goshdarngames@users.noreply.github.com>
2020-05-21 11:03:03 +0100

    Test onmessage inside namespace

--- javascript/traverse_prolog_worker.js
+++ javascript/traverse_prolog_worker.js
@@ -1,7 +1,11 @@
-
-onmessage = function ( e )
+( function ( traverse, undefined )
 {
-    console.log ( "Thread received message" );
-    postMessage ( e.data );
-};
+    traverse.prolog_worker_message = function ( e )
+    {
+        console.log ( "Thread received message" );
+        postMessage ( e.data );
+    };
+} ( self.traverse = self.traverse || {} ))
+
+onmessage = ( e ) => traverse.prolog_worker_message ( e );
 
