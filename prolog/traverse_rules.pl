7c491e39f7367c49be565d1dc361b8e5d57ebdd8
Gosh Darn Games <1109935+goshdarngames@users.noreply.github.com>
2020-05-25 20:24:03 +0100

    Additional prolog rules

--- prolog/traverse_rules.pl
+++ prolog/traverse_rules.pl
@@ -1,19 +1,28 @@
-%gridsize
-width(16).
-height(16).
-
-%puzzle objects
+/**
+ * Puzzle objects
+ */
 object( wall ).
 object( bogey ).
 object( boo ).
 
-%only allowed one of these in each puzzle
+/**
+ * Only allowed one of these in each puzzle
+ */
 unique( boo ).
 unique( bodey ).
 
-%must be at least one of these in each puzzle
+/**
+ * Must be at least one of these in each puzzle
+ */
 required( boo ).
 required( bogey ).
+
+/**
+ * Space in which puzzle objects can move
+ */
+
+width(16).
+height(16).
 
 position( [ X, Y ] ) :-
     width(W),
@@ -23,20 +32,61 @@ position( [ X, Y ] ) :-
     Y>=0,
     Y<H.
 
-not_member(_,[]).
+direction(up).
+direction(down).
+direction(left).
+direction(right).
 
+/**
+ * States objects can be in.
+ */
+
+state([static]).
+state([moving,Dir]):-
+    direction(Dir).
+
+/**
+ * This tuple represents each object in a puzzle state.
+ */
+state_position_object([Ob,Pos,State]):
+    object(Ob),
+    position(Pos),
+    state(State).
+
+/**
+ * Inputs player can make.
+ */
+
+player_controlled( boo ).
+player_controlled( bogey ).
+
+input( Ob, Dir ) :-
+    player_controlled( Ob ),
+    direction( D ).
+
+%Automatic actions 
+input(none). 
+
+/**
+ * Utility: True if element is not member of list
+ */
+
+not_member(_,[]).
 not_member(X,[Head|Tail]) :-
     X \= Head,
     not_member( X, Tail ).
 
+/**
+ * Relates a puzzle state with a string describing a problem
+ * that prevents the state from being valid.
+ */
 puzzle_problem( [], "No objects").
-
 puzzle_problem( P, "Boo Missing"):-
     not_member([boo|_],P).
-
 puzzle_problem( P, "Bogey Missing"):-
     not_member([bogey|_],P).
-
 puzzle_problem(_,"None").
 
-
+/**
+ * Relating puzzle states and inputs.
+ */
