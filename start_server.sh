ec6d0ab75298b0f4acfd24be107d9e03a6c00176
Gosh Darn Games <1109935+goshdarngames@users.noreply.github.com>
2020-08-06 15:48:33 +0100

    Comment

--- start_server.sh
+++ start_server.sh
@@ -1,1 +1,2 @@
+#Starts an apache docker container to host the game page
 sudo docker run -dit --name traverse-apache-app -p 8080:80 -v /home/boop/Documents/GoshDarnGames/Traverse2:/usr/local/apache2/htdocs/ httpd:2.4
