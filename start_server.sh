a89d2d6fcad73ced968ee2a2425623167b43dbcf
Gosh Darn Games <1109935+goshdarngames@users.noreply.github.com>
2020-08-06 18:43:19 +0100

    Stop existing server

--- start_server.sh
+++ start_server.sh
@@ -1,2 +1,4 @@
 #Starts an apache docker container to host the game page
-sudo docker run -dit --name traverse-apache-app -p 8080:80 -v /home/boop/Documents/GoshDarnGames/Traverse2:/usr/local/apache2/htdocs/ httpd:2.4
+sudo docker stop traverse-apache-app
+sudo docker rm traverse-apache-app
+sudo docker run -dit --name traverse-apache-app -p 1337:80 -v /home/boop/Documents/GoshDarnGames/Traverse2:/usr/local/apache2/htdocs/ httpd:2.4
